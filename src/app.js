48e9696da239e684d212de74b643f2e1521634e7
Ashtn <ashtonehorton@gmail.com>
2017-05-30 16:50:18 -0700

    wave 1 complete, with ability to display instance of model

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
 import $ from 'jquery';
+import _ from 'underscore';
+// links contact model to application js file
+import Contact from './app/models/contact.js';
+
+
+// new instance of contact model with static data, this is just for testing purposes
+var myContact = new Contact({
+  name: 'Ashton',
+  email: 'ashton@ashton.com',
+  phoneNumber: '909-000-0000'
+});
+
+// NOTE notice the use of closures here
+var render = function(contact){
+  // selected the html that lives with the tmpl-contact-card and assigning to the var templateText
+  var templateText = $('#tmpl-contact-card').html();
+
+  // create underscore template using the previously generated templateText (think of it as creating a new instance of underscore)
+  var templateObject = _.template(templateText);
+
+// compile object data into underscore template oject
+  var compiledHTML = templateObject(contact.toJSON());
+
+  //append
+  $('#contact-cards').append(compiledHTML);
+};
+
+$(document).ready(function() {
+  console.log('heyyyy');
+  render(myContact);
+});
