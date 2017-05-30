10d6251571fb882f49634b59e8a869c6e8c05a67
Chris M <chris@adadevelopersacademy.org>
2017-05-30 15:39:59 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,1 @@
-import Application from 'app/models/application';
-import ApplicationView from 'app/views/application_view';
-
-var application = new Application();
-
-var appView = new ApplicationView({
-  el: '#application',
-  model: application
-});
+import $ from 'jquery';
