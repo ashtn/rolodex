e970e603de7b3f16de0e6577e5f17896e63d48c0
Ashtn <ashtonehorton@gmail.com>
2017-06-01 20:24:46 -0700

    modal displays with name, email and phone number

--- src/app.js
+++ src/app.js
@@ -2,31 +2,57 @@ import $ from 'jquery';
 import _ from 'underscore';
 // links contact model to application js file
 import Contact from './app/models/contact.js';
+import Rolodex from './app/collections/rolodex.js';
 
+import ContactView from './app/views/contact_view.js';
+import RolodexView from './app/views/rolodex_view.js';
 
 // new instance of contact model with static data, this is just for testing purposes
-var myContact = new Contact({
+var myContact = new Contact(
+  {
   name: 'Ashton',
   email: 'ashton@ashton.com',
-  phoneNumber: '909-000-0000'
-});
+  phone: '909-000-0000'
+}
+// {
+//   name: 'Test',
+//   email: 'Test@test.com',
+//   phoneNumber: '000-000-0000'
+// }
+);
 
 // NOTE notice the use of closures here
-var render = function(contact){
-  // selected the html that lives with the tmpl-contact-card and assigning to the var templateText
-  var templateText = $('#tmpl-contact-card').html();
+// var render = function(contact){
+//   // selected the html that lives with the tmpl-contact-card and assigning to the var templateText
+//   var templateText = $('#tmpl-contact-card').html();
+//
+//   // create underscore template using the previously generated templateText (think of it as creating a new instance of underscore)
+//   var templateObject = _.template(templateText);
+//
+// // compile object data into underscore template oject
+//   var compiledHTML = templateObject(contact.toJSON());
+//
+//   //append
+//   $('#contact-cards').append(compiledHTML);
+// };
 
-  // create underscore template using the previously generated templateText (think of it as creating a new instance of underscore)
-  var templateObject = _.template(templateText);
+// new instance of Rolodex (collection)
+var myRolodex = new Rolodex(myContact);
 
-// compile object data into underscore template oject
-  var compiledHTML = templateObject(contact.toJSON());
 
-  //append
-  $('#contact-cards').append(compiledHTML);
-};
 
 $(document).ready(function() {
-  console.log('heyyyy');
-  render(myContact);
+  // console.log('heyyyy');
+  // render(myContact);
+// the object shows up in params
+  var myRolodexView = new RolodexView({
+    model: myRolodex,
+    // this goes to rolodex_view.js
+    // _.template() returns a functoin
+    template: _.template($('#tmpl-contact-card').html()),
+    // setting el to be main?
+    // because there is one list, we can assign el to main
+    el: 'body'
+  });
+  myRolodexView.render();
 });
---
+++ src/app/views/rolodex_view.js
@@ -0,0 +1,83 @@
+import Backbone from 'backbone';
+import $ from 'jquery';
+import _ from 'underscore';
+import Contact from '../models/contact.js';
+import ContactView from '../views/contact_view.js';
+// import ContactDetailsView from '../views/contact_details_view.js';
+
+var RolodexView = Backbone.View.extend({
+  initialize: function(params){
+    this.contactTemplate = params.template;
+    this.listenTo(this.model, "update", this.render);
+    this.modalTemplate = _.template($('#tmpl-contact-details').html());
+  },
+  render: function(){
+    // clears something
+
+    this.$('#contact-cards').empty();
+    // console.log('this.contactTempalte: ', this.contactTemplate);
+    // console.log('this.modalTemplate: ', this.modalTemplate);
+    var that = this;
+
+    this.model.each(function(contact){
+      var contactView = new ContactView({
+        model: contact,
+        myTemplate: that.contactTemplate, // gives it to contact view
+        // tagName: ''
+      });
+      that.listenTo(contactView, "selectedCard", that.displayModal);
+      // rendered the view and append the new view to the 'todo-items'
+      that.$('#contact-cards').append(contactView.render().$el);
+    });
+    //we `return this` so that we can `myView.render().el`
+    return this;
+  },
+  events: {
+    //listens for the click on the save
+    'click .btn-save': 'saveContact',
+    'click .btn-cancel': 'clearContact',
+    // 'click .contact-card': 'viewContact'
+  },
+  getInputData: function(){
+    var inputName = this.$('#name').val(); this.$('#name').val('');
+    // var inputName = this.$('label[name=name]').val(); this.$('lable[name=name]').val('');
+    var inputEmail = this.$('#email').val(); this.$('#email').val('');
+    // var inputEmail = this.$('label[name=email]').val(); this.$('lable[name=name]').val('');
+    var inputPhone = this.$('#phone').val(); this.$('#phone').val('');
+    // var inputPhone = this.$('label[name=phone]').val(); this.$('lable[name=name]').val('');
+
+    return {
+      name: inputName,
+      email: inputEmail,
+      phone: inputPhone,
+    };
+  },
+  // creates new contact model object with the information passed from the form
+  saveContact: function(){
+    var contact = new Contact(this.getInputData());
+    console.log('add contact-buttion');
+    //adds the new contact model to the rolodex collection
+    // this add triggers the `update` listener above
+    this.model.add(contact);
+  },
+  clearContact: function(){
+    this.$('#name').val('');
+    this.$('#email').val('');
+    this.$('#phone').val('');
+  },
+
+  displayModal: function(contact){
+    // console.log('contact: ', contact);
+    // console.log('contact.toJSON: ',contact.toJSON());
+    $('#contact-details').empty();
+
+    // var modalDetails = this.
+    var generatedModalTemplate = this.modalTemplate(contact.attributes);
+
+    this.$('#contact-details').append(generatedModalTemplate);
+
+    return this;
+  }
+});
+
+export default RolodexView;
